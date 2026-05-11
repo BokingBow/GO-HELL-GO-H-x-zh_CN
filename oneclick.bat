81557f0a23bf0b4169539aeb1f8c643e4ac82284
BokingBow <85831678+BokingBow@users.noreply.github.com>
2026-05-11 20:12:35 +0800

    添加 oneclick.bat 脚本以自动化打包过程

---
+++ oneclick.bat
@@ -0,0 +1,5 @@
+python ./to/ready.py
+python ./qu.py
+python ./list.py
+
+"E:\AAA\Epic Games\UE_4.27\Engine\Binaries\Win64\UnrealPak.exe" "C:\Users\Boking Bow\Game\gohellgo\gohellgo\Content\Paks\ChineseTest_P.pak" -Create="F:\Reason\Origin\GO HELL GO 素材\FileList.txt" -compress
